6196d7e09a65a5c92660a3a10118b7d3eb332175
Hongseok Choi <hs.choi@kimiro.re.kr>
2021-02-23 19:47:22 +0900

    Unix Linux 유틸리티 MAKE 이지 마스터 가이드 / 김범준 / 코드앤컨텐츠
    4.2장 서브 디렉터리 처리

---
+++ CMake/kim_Codeandcontents/Practice/ch04/subdir/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "hamburger.h"
+#include "steak.h"
+#include "cola.h"
+#include "cider.h"
+
+
+int main()
+{
+	printf("This is main\n");
+	hamburger();
+	steak();
+	cola();
+	cider();
+}
