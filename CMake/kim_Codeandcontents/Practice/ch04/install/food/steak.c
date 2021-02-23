f997f0a4ad6f87dcc8bcd8fd1848f1a733d54d63
Hongseok Choi <hs.choi@kimiro.re.kr>
2021-02-23 22:11:50 +0900

    Unix Linux 유틸리티 MAKE 이지 마스터 가이드 / 김범준 / 코드앤컨텐츠
    4.2장 파일을 시스템에 설치하기
    cleanup 프로젝트 빌드에러남; OS가 달라서 명령어 에러나는 듯.

---
+++ CMake/kim_Codeandcontents/Practice/ch04/install/food/steak.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void steak()
+{
+	printf("This is a steak.\n");
+}
---
+++ CMake/kim_Codeandcontents/Practice/ch04/install/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "hamburger.h"
+#include "steak.h"
+
+int main()
+{
+	printf("This is main\n");
+	hamburger();
+	steak();
+}
