6fc679086cad2fc9bfe3c3e3af0246cfcfd179e9
Hongseok Choi <hs.choi@kimiro.re.kr>
2021-03-08 13:51:57 +0900

    OpenCV 4로 배우는 컴퓨터 비전과 머신 러닝
    최초 커밋

---
+++ CV/hwang_gilbutITbook/Practice/ch02/dispBMP/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+                                        //
+
+
+int main()
+{
+    std::cout << "Hello OpenCV " << CV_VERSION << std::endl;
+
+    cv::Mat img;
+    img = cv::imread("F:/98_BookStudy/CV/hwang_gilbutITbook/Data/images/lenna.bmp");
+
+    if (img.empty()) {
+        std::cerr << "Image load failed!" << std::endl;
+        return -1;
+    }
+
+    cv::namedWindow("image");
+    cv::imshow("image", img);
+
+    cv::waitKey();
+
+    return 0;
+}
---
+++ CV/hwang_gilbutITbook/Practice/ch02/helloCV/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+                                        //
+
+
+int main()
+{
+    std::cout << "Hello OpenCV " << CV_VERSION << std::endl;
+    return 0;
+}
